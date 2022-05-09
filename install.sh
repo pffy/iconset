f5d6528f79755549b9b7ecc7375c60a913036fb1
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-05-09 16:20:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,11 @@
 
 mkdir -p ~/pffypasta
 cp iconset.php ~/pffypasta/iconset
+echo "adding to ~/pffypasta ..."
 sudo chmod u+x ~/pffypasta/iconset
+
+echo "setting up iconset command ..."
 sudo ln -sf ~/pffypasta/iconset /usr/local/bin/iconset
+
+echo "done."
+iconset
